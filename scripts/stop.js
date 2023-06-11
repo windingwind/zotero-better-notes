84d555f54b0235e22487e365a1d75a3b09a12547
windingwind <wyzlshx@foxmail.com>
2023-06-11 15:50:19 +0800

    fix: scripts/stop

--- scripts/stop.js
+++ scripts/stop.js
@@ -1,10 +1,32 @@
 const { execSync } = require("child_process");
 const { killZoteroWindows, killZoteroUnix } = require("./zotero-cmd.json");
 
+const MAX_WAIT_TIME = 10000;
+
+const startTime = new Date().getTime();
+
 try {
   if (process.platform === "win32") {
     execSync(killZoteroWindows);
+
+    // wait until zotero.exe is fully stopped. maximum wait for 10 seconds
+    while (new Date().getTime() - startTime <= MAX_WAIT_TIME) {
+      try {
+        execSync('tasklist | find /i "zotero.exe"');
+      } catch (e) {
+        break;
+      }
+    }
   } else {
     execSync(killZoteroUnix);
+
+    // wait until zotero is fully stopped. maximum wait for 10 seconds
+    while (new Date().getTime() - startTime <= MAX_WAIT_TIME) {
+      try {
+        execSync("ps aux | grep -i zotero");
+      } catch (e) {
+        break;
+      }
+    }
   }
 } catch (e) {}
