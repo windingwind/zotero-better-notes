753f4e8e9ca7edaf80e4bd069c93e81be4fe0ea4
windingwind <33902321+windingwind@users.noreply.github.com>
2024-03-08 21:20:51 +0800

    fix: reader annotation icon memory leakage

--- src/modules/reader.ts
+++ src/modules/reader.ts
@@ -43,7 +43,7 @@ export function registerReaderAnnotationButton() {
               },
             },
           ],
-          enableElementRecord: true,
+          enableElementRecord: false,
         }),
       );
     },
