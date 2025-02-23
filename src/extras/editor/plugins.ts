d168147ccf5f4b6e4079cc62286f441bd3a544bd
windingwind <33902321+windingwind@users.noreply.github.com>
2025-02-23 22:27:46 +0100

    add: table resize and head pinning

--- src/extras/editor/plugins.ts
+++ src/extras/editor/plugins.ts
@@ -1,6 +1,10 @@
 import { initLinkPreviewPlugin, LinkPreviewOptions } from "./linkPreview";
 import { initMagicKeyPlugin, MagicKeyOptions } from "./magicKey";
 import { initMarkdownPastePlugin, MarkdownPasteOptions } from "./markdownPaste";
+import { EditorView } from "prosemirror-view";
+// Use custom column resizing plugin, since the original one breaks
+import { columnResizing } from "./columnResizing";
+import { TableView } from "prosemirror-tables";
 
 export { initPlugins };
 
@@ -21,7 +25,28 @@ function initPlugins(options: {
   plugins = initMagicKeyPlugin(plugins, options.magicKey);
   // Collect all plugins and reconfigure the state only once
   const newState = core.view.state.reconfigure({
-    plugins,
+    plugins: [
+      ...plugins,
+      columnResizing({
+        cellMinWidth: 80,
+        handleWidth: 5,
+      }),
+    ],
   });
+
+  initNodeViews(core.view);
+
   core.view.updateState(newState);
+  // Rerender the view
+  core.view.dispatch(core.view.state.tr.setMeta("force-update", true));
+}
+
+function initNodeViews(view: EditorView) {
+  // @ts-ignore
+  const tableNodeViewProto = view.nodeViews.table().__proto__;
+
+  // Patch the prototype to use the table node view from prosemirror-tables
+  tableNodeViewProto.update = TableView.prototype.update;
+  tableNodeViewProto.ignoreMutation = TableView.prototype.ignoreMutation;
+  tableNodeViewProto.constructor = TableView;
 }
