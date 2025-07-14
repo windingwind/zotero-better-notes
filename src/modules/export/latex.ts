35c64d3f7a18abff3ca9e3beac7a35b3012320dd
flovst <61141602+flovst@users.noreply.github.com>
2025-07-14 17:08:28 +0800

    add: support for ${filename} in specifying attachment folder (#356)

--- src/modules/export/latex.ts
+++ src/modules/export/latex.ts
@@ -3,32 +3,25 @@ import { getPref } from "../../utils/prefs";
 import { formatPath, jointPath } from "../../utils/str";
 
 export async function saveLatex(
-  filename: string,
+  filePath: string,
   noteId: number,
   options: {
     keepNoteLink?: boolean;
   } = {},
 ) {
   const noteItem = Zotero.Items.get(noteId);
-  const dir = jointPath(...PathUtils.split(formatPath(filename)).slice(0, -1));
+  const filename = PathUtils.split(filePath).pop()
+  const dir = jointPath(...PathUtils.split(formatPath(filePath)).slice(0, -1));
   await IOUtils.makeDirectory(dir);
-  const hasImage = noteItem.getNote().includes("<img");
-  if (hasImage) {
-    const attachmentsDir = jointPath(
-      dir,
-      getPref("syncAttachmentFolder") as string,
-    );
-    await IOUtils.makeDirectory(attachmentsDir);
-  }
   const [latexContent, bibString] = await addon.api.convert.note2latex(
     noteItem,
     dir,
-    options,
+    {...options, filename: filename},
   );
-  await Zotero.File.putContentsAsync(filename, latexContent);
+  await Zotero.File.putContentsAsync(filePath, latexContent);
 
-  showHintWithLink(`Note Saved to ${filename}`, "Show in Folder", (ev) => {
-    Zotero.File.reveal(filename);
+  showHintWithLink(`Note Saved to ${filePath}`, "Show in Folder", (ev) => {
+    Zotero.File.reveal(filePath);
   });
 
   if (bibString && bibString.length > 0) {
@@ -54,18 +47,12 @@ export async function saveLatex(
   }
 }
 
-export async function saveMergedLatex(filename: string, noteIds: number[]) {
+export async function saveMergedLatex(filePath: string, noteIds: number[]) {
   const noteItems = noteIds.map((noteId) => Zotero.Items.get(noteId));
-  const dir = jointPath(...PathUtils.split(formatPath(filename)).slice(0, -1));
+  const filename = PathUtils.split(filePath).pop()
+  const dir = jointPath(...PathUtils.split(formatPath(filePath)).slice(0, -1));
   await IOUtils.makeDirectory(dir);
-  const hasImage = noteItems.some((item) => item.getNote().includes("<img"));
-  if (hasImage) {
-    const attachmentsDir = jointPath(
-      dir,
-      getPref("syncAttachmentFolder") as string,
-    );
-    await IOUtils.makeDirectory(attachmentsDir);
-  }
+  
   let latexContent = "";
   let bibString = "";
   const separatedString = "\n\n";
@@ -73,7 +60,7 @@ export async function saveMergedLatex(filename: string, noteIds: number[]) {
     const [latexContent_, bibString_] = await addon.api.convert.note2latex(
       noteItem,
       dir,
-      {},
+      {filename: filename},
     );
     latexContent += latexContent_;
     latexContent += separatedString;
@@ -83,10 +70,10 @@ export async function saveMergedLatex(filename: string, noteIds: number[]) {
     }
   }
 
-  await Zotero.File.putContentsAsync(filename, latexContent);
+  await Zotero.File.putContentsAsync(filePath, latexContent);
 
-  showHintWithLink(`Note Saved to ${filename}`, "Show in Folder", (ev) => {
-    Zotero.File.reveal(filename);
+  showHintWithLink(`Note Saved to ${filePath}`, "Show in Folder", (ev) => {
+    Zotero.File.reveal(filePath);
   });
 
   if (bibString.length > 0) {
